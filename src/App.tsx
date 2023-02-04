33fa8aab8206d324d08ca71ce9a1efdc1becb9a9
Rajasri Janaki Raman <rajasri215@gmail.com>
2023-02-04 08:19:10 +0000

    Update to HashRouter to support github pages hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import "./styles.css";
 
 export default function App() {
   return (
-    <Router>
+    <div>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
