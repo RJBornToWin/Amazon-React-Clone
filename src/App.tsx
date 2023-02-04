70f3220cd16c5ebcfd00c7e57aab32236208a15f
Rajasri Janaki Raman <rajasri215@gmail.com>
2023-02-04 07:33:25 +0000

    Update 14 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Header from "./components/Header";
 import "./styles.css";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
